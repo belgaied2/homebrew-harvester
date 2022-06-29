6a676702721a024eb34385e6e89e9c8bf5a2c75b
Mohamed Belgaied Hassine <60746935+belgaied2@users.noreply.github.com>
2022-06-29 14:59:30 +0200

    small improvement to the Formula for build-from-source installation

--- Formula/harvester.rb
+++ Formula/harvester.rb
@@ -1,32 +1,20 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Harvester < Formula
-  desc "Harvester CLI is a project that aims to provide an opinionated CLI tool to easily manage VMs on Harvester, the promising HCI solution created by Rancher Labs/SUSE"
-  homepage "https://github.com/belgaied2/harvester-cli/"
-  url "https://github.com/belgaied2/harvester-cli/releases/download/v0.1.0/harvester-v0.1.0-darwin-arm64.tar.gz"
-  version "0.1.0"
-  sha256 "1785f3af8e7eea24ad539a9e03d8a3da71905b8e6da23816a671eaf070525496"
-  license "GPL-3"
+  desc "CLI tool to manage VMs on Harvester"
+  homepage "https://github.com/belgaied2/harvester-cli"
+  url "https://github.com/belgaied2/harvester-cli.git",
+      tag:      "v0.1.0",
+      revision: "c2962916b7e379b609c5092247ec28f0c8875a09"
+  license "Apache-2.0"
 
-  # depends_on "go" => :build
+  depends_on "go" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # system "go", "build", *std_go_args(ldflags: "-s -w")
-    bin.install "harvester"
+
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.VERSION=#{version}")
   end
 
-  # test do
-  #   # `test do` will create, run in and delete a temporary directory.
-  #   #
-  #   # This test will fail and we won't accept that! For Homebrew/homebrew-core
-  #   # this will need to be a test that verifies the functionality of the
-  #   # software. Run the test with `brew test harvester`. Options passed
-  #   # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-  #   #
-  #   # The installed folder is not in the path, so use the entire path to any
-  #   # executables being tested: `system "#{bin}/program", "do", "something"`.
-  #   system "false"
-  # end
+  test do
+    output = shell_output("#{bin}/harvester login https://localhost -t token 2>&1", 1).split("\n").pop[/msg=\"(.*)\"/, 1]
+    assert_match "invalid token", output
+  end
 end
