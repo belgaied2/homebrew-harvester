6cd703ac91d542b716c508da6cfe2e93c3c32f20
Mohamed Belgaied Hassine <belgaied2@hotmail.com>
2022-07-08 09:48:08 +0200

    Adding bottle information for arm64 monterey

--- Formula/harvester.rb
+++ Formula/harvester.rb
@@ -10,6 +10,7 @@ class Harvester < Formula
     root_url "https://github.com/belgaied2/homebrew-harvester/releases/download/harvester-0.1.1"
     rebuild 1
     sha256 cellar: :any_skip_relocation, big_sur: "1d5f3f6fca5f0786d2985c32178ce89d95751d464174ffa2ad6e832bb5a94f59"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "d05668392f5644f2f6b99f3d52ab80b94a50b052f16bdbf396bae3f1d13f965b"
   end
 
   depends_on "go" => :build
